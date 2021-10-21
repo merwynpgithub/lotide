e5d580bd1dc2da3eaad7415618698c6373b43e92
Merwyn Patrick <merwynpatrick@Merwyns-Mac-mini.local>
2021-10-21 14:39:43 -0400

    added head file to test consoleAssert with array

---
+++ head.js
@@ -0,0 +1,14 @@
+const assertEqual = function(actual, expected) {
+  let result;
+  //using emoji
+  let right = String.fromCodePoint(0x2713);
+  let wrong = String.fromCodePoint(0x2717);
+  actual !== expected ? result = `${wrong} Assertion Failed: ${actual} !== ${expected}` : result = `${right} Assertion Passed: ${actual} === ${expected}`;
+  console.log(result);
+};
+
+const head = function(arr) {
+  return arr;
+};
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
