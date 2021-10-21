df2fbb2d7659559c9794432a173a1483a135b797
Merwyn Patrick <merwynpatrick@Merwyns-Mac-mini.local>
2021-10-21 12:52:51 -0400

    setting up our own consoleAssert

---
+++ assertEqual.js
@@ -0,0 +1,13 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  let result;
+  //using emoji
+  let right = String.fromCodePoint(0x2713);
+  let wrong = String.fromCodePoint(0x2717);
+  actual !== expected ? result = `${wrong} Assertion Failed: ${actual} !== ${expected}` : result = `${right} Assertion Passed: ${actual} === ${expected}`;
+  console.log(result);
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
