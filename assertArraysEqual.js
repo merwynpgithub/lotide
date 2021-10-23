e15dac83f761d295ba08699f7e00a3859d7e0b8c
Merwyn Patrick <merwynpatrick@Merwyns-Mac-mini.local>
2021-10-23 09:49:20 -0400

    added file to test 2 arrays equality and display proper message

---
+++ assertArraysEqual.js
@@ -0,0 +1,23 @@
+const eqArrays = function(actual, expected) {
+  return actual === expected || JSON.stringify(actual) === JSON.stringify(expected);
+};
+
+const assertArraysEqual = function(arr1, arr2) {
+  const result = eqArrays(arr1, arr2);
+  //using emoji
+  let right = String.fromCodePoint(0x2713);
+  let wrong = String.fromCodePoint(0x2717);
+
+  let answer1, answer2;
+  // result === true ? answer = `${right} Assertion Passed: ` : answer = `${wrong} Assertion Failed: `;
+  if (result) {
+    answer1 = `${right} Assertion Passed: `;
+    answer2 = `===`;
+  } else {
+    answer1 = `${wrong} Assertion Failed: `;
+    answer2 = `!==`;
+  }
+  console.log(answer1, arr1, answer2, arr2);
+  // return null;
+};
+console.log(assertArraysEqual(["1", "2", "3"], ["1", "2", 3]));
