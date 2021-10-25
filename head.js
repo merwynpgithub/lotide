9b75e51669dbcc80b5a9d733f54922adc0999046
Merwyn Patrick <merwynpatrick@Merwyns-Mac-mini.local>
2021-10-25 11:35:04 -0400

    updated head file to include return value for empty array passed

--- head.js
+++ head.js
@@ -8,6 +8,9 @@ const assertEqual = function(actual, expected) {
 };
 
 const head = function(arr) {
+  if (arr.length === 0) {
+    return undefined;
+  }
   return arr;
 };
 assertEqual(head([5,6,7]), 5);
