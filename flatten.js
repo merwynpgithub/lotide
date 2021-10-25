a31199aa0b201edd31ae58f7b9d8ff1a1387584b
Merwyn Patrick <merwynpatrick@Merwyns-Mac-mini.local>
2021-10-25 10:45:39 -0400

    implement lodash flatten function

---
+++ flatten.js
@@ -0,0 +1,16 @@
+const flatten = function(arr) {
+  let result = [];
+  if (!Array.isArray(arr)) return "Not an array";
+  for (let i = 0; i <  arr.length; i++) {
+    if (!Array.isArray(arr[i])) {
+      result.push(arr[i]);
+    } else {
+      for (let j = 0; j < arr[i].length; j++) {
+        result.push(arr[i][j]);
+      }
+    }
+  }
+  return result;
+};
+
+console.log(flatten([1, 2, [3, 4], 5, [6]]));
