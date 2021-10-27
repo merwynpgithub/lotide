df26c2d56adcaade034d85a42deb7c9792fa2ae9
Merwyn Patrick <merwynpatrick@Merwyns-Mac-mini.local>
2021-10-27 08:26:41 -0400

    Implementing lodash middle and added assertEqual

--- middle.js
+++ middle.js
@@ -1,3 +1,16 @@
+const assertEqual = function(actual, expected) {
+  let result;
+  //using emoji
+  let right = String.fromCodePoint(0x2713);
+  let wrong = String.fromCodePoint(0x2717);
+  actual !== expected ? result = `${wrong} Assertion Failed: ${actual} !== ${expected}` : result = `${right} Assertion Passed: ${actual} === ${expected}`;
+  console.log(result);
+};
+
+const eqArrays = function(actual, expected) {
+  return actual === expected || JSON.stringify(actual) === JSON.stringify(expected);
+};
+
 const middle = function(arr) {
   let result = [];
   if (arr.length > 2) {
