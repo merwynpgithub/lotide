450a5615646713e817cc9df82c0c891188e6afe9
Merwyn Patrick <merwynpatrick@Merwyns-Mac-mini.local>
2021-11-09 14:34:08 -0500

    added eqArraysTest

--- eqArrays.js
+++ eqArrays.js
@@ -1,14 +1,5 @@
-const assertEqual = function(actual, expected) {
-  let result;
-  //using emoji
-  let right = String.fromCodePoint(0x2713);
-  let wrong = String.fromCodePoint(0x2717);
-  actual !== expected ? result = `${wrong} Assertion Failed: ${actual} !== ${expected}` : result = `${right} Assertion Passed: ${actual} === ${expected}`;
-  console.log(result);
-};
-
 const eqArrays = function(actual, expected) {
   return actual === expected || JSON.stringify(actual) === JSON.stringify(expected);
 };
 
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), true);
+module.exports = eqArrays;
