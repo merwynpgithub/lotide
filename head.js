9c5600a2442b9ca02153548b748f245913ae9a71
Merwyn Patrick <merwynpatrick@Merwyns-Mac-mini.local>
2021-11-09 13:03:48 -0500

    Refactoring code, using modules and separating tests into separate files

--- head.js
+++ head.js
@@ -1,17 +1,8 @@
-const assertEqual = function(actual, expected) {
-  let result;
-  //using emoji
-  let right = String.fromCodePoint(0x2713);
-  let wrong = String.fromCodePoint(0x2717);
-  actual !== expected ? result = `${wrong} Assertion Failed: ${actual} !== ${expected}` : result = `${right} Assertion Passed: ${actual} === ${expected}`;
-  console.log(result);
-};
-
 const head = function(arr) {
   if (arr.length === 0) {
     return undefined;
   }
   return arr;
 };
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+
+module.exports = head;
