f814873ee620beb6a6d89b456b49b812acd50169
Merwyn Patrick <merwynpatrick@Merwyns-Mac-mini.local>
2021-11-10 11:57:20 -0500

    added index file to import all lotide functions

--- flatten.js
+++ flatten.js
@@ -13,4 +13,5 @@ const flatten = function(arr) {
   return result;
 };
 
+module.exports = flatten;
 console.log(flatten([1, 2, [3, 4], 5, [6]]));
---
+++ index.js
@@ -0,0 +1,15 @@
+const head = require('./head');
+const tail = require('./tail');
+const middle = require('./middle');
+const flatten = require('./flatten');
+const assertArraysEqual = require('./assertArraysEqual');
+const assertEqual = require('./assertEqual');
+
+module.exports = {
+  head: head,
+  tail: tail,
+  middle: middle,
+  flatten: flatten,
+  assertArraysEqual: assertArraysEqual,
+  assertEqual: assertEqual
+};
