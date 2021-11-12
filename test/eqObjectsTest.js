8428aca16a984a78db3cda87839cfa3b060e1175
Merwyn Patrick <merwynpatrick@Merwyns-Mac-mini.local>
2021-11-12 17:13:01 -0500

    updated eqObjectsTest

--- test/eqObjectsTest.js
+++ test/eqObjectsTest.js
@@ -5,18 +5,18 @@ describe('#eqObjects', () => {
   it('should compare 2 objects and return a boolean', () => {
     const ab = { a: "1", b: "2" };
     const ba = { b: "2", a: "1" };
-    assert.deepEqual(ab, ba);
+    assert.deepEqual(_.eqObjects(ab, ba), true);
   });
 
   it('should compare 2 objects and return a boolean', () => {
     const ab = { c: "1", d: ["2", 3] };
     const ba = { d: ["2", 3], c: "1" };
-    assert.deepEqual(ab, ba);
+    assert.deepEqual(_.eqObjects(ab, ba), true);
   });
 
   it('should compare 2 objects and return a boolean', () => {
     const ab = { c: "1", d: ["2", 3] };
     const ba = { c: "1", d: ["2", 3, 4] };
-    assert.deepEqual(ab, ba);
+    assert.deepEqual(_.eqObjects(ab, ba), false);
   });
 });
