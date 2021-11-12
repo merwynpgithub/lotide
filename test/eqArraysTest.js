3b838c08806ba057ef674d1351e02f0c1788c00e
Merwyn Patrick <merwynpatrick@Merwyns-Mac-mini.local>
2021-11-12 17:30:00 -0500

    updated eqArraysTest

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -1,12 +1,13 @@
-const { eqArrays, eqArraysRecursion } = require('../eqArrays');
+// const { eqArrays, eqArraysRecursion } = require('../eqArrays');
 const assert = require('chai').assert;
+const _ = require('../index');
 
 describe('#eqArrays', () => {
   it('should return true/false for comparing 2 deeply nested arrays using recursions', () => {
-    assert.deepEqual(eqArraysRecursion([1, [2, [5, 6]], 3], [1, [2, [5, 6]], 3]), true);
+    assert.deepEqual(_.eqArraysRecursion([1, [2, [5, 6]], 3], [1, [2, [5, 6]], 3]), true);
   });
 
   it('should return a boolean for comparing 2 arrays', () => {
-    assert.deepEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
+    assert.deepEqual(_.eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
   });
 });
