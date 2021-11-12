79fd6bc9c02f53f73c7c98e9cd290eff063b225f
Merwyn Patrick <merwynpatrick@Merwyns-Mac-mini.local>
2021-11-12 13:35:19 -0500

    updated eqArrays with recursion

--- eqArrays.js
+++ eqArrays.js
@@ -1,5 +1,19 @@
+//Method 1: using JSON.stringify
 const eqArrays = function(actual, expected) {
   return actual === expected || JSON.stringify(actual) === JSON.stringify(expected);
 };
 
-module.exports = eqArrays;
+//Method 2: using recursion:
+const eqArraysRecursion = function(actual, expected) {
+  if (actual.length !== expected.length) return false;
+  for (let i = 0; i < expected.length; i++) {
+    if (Array.isArray(actual[i]) && Array.isArray(expected[i])) {
+      let answer = eqArraysRecursion(actual[i], expected[i]);
+      if (!answer) return false;
+    } else {
+      if (actual[i] !== expected[i]) return false;
+    }
+  }
+  return true;
+};
+module.exports = { eqArrays, eqArraysRecursion };
--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -1,4 +1,5 @@
 const assertEqual = require('../assertEqual');
-const eqArrays = require('../eqArrays');
+const { eqArrays, eqArraysRecursion } = require('../eqArrays');
 
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), true);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
+assertEqual(eqArraysRecursion([1, [2, [5, 6]], 3], [1, [2, [5, 6]], 3]), false);
