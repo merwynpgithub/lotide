e01f60b5f89fafec3929f3560fe7f0dc9f34614c
Merwyn Patrick <merwynpatrick@Merwyns-Mac-mini.local>
2021-11-12 17:09:14 -0500

    added eqObjects

--- eqObjects.js
+++ eqObjects.js
@@ -34,17 +34,3 @@ const eqObjects = function (object1, object2) {
   return true;
 };
 module.exports = eqObjects;
-
-// const ab = { a: "1", b: "2" };
-// const ba = { b: "2", a: "1" };
-// eqObjects(ab, ba); // => true
-
-// const abc = { a: "1", b: "2", c: "3" };
-// eqObjects(ab, abc); // => false
-
-// const cd = { c: "1", d: ["2", 3] };
-// const dc = { d: ["2", 3], c: "1" };
-// eqObjects(cd, dc); // => true
-
-// const cd2 = { c: "1", d: ["2", 3, 4] };
-// eqObjects(cd, cd2); // => false
