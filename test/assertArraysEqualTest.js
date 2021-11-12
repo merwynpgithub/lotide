a98f12256a2bc00a2b4389848d4d38a5be6afb0f
Merwyn Patrick <merwynpatrick@Merwyns-Mac-mini.local>
2021-11-12 15:40:59 -0500

    updated assertArraysEqualTest

--- test/assertArraysEqualTest.js
+++ test/assertArraysEqualTest.js
@@ -1,2 +1,7 @@
-const assertArraysEqual = require('../assertArraysEqual');
-console.log(assertArraysEqual(["1", "2", "3"], ["1", "2", 3]));
+const assert = require('chai').assert;
+
+describe('#assertArraysEqual', () => {
+  it('should return true/false for comparing 2 arrays using deepEqual', () => {
+    assert.deepEqual(["1", "2", "3"], ["1", "2", "3"]);
+  });
+});
